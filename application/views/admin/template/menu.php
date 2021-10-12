6c2a629d1408d217776918df921fe9d250a7654f
tonialfiyan1 <tonialfiyan1@gmail.com>
2021-10-12 08:59:26 +0700

    add icon menu master

--- application/views/admin/template/menu.php
+++ application/views/admin/template/menu.php
@@ -3,20 +3,20 @@
 
                 <!-- START: Menu-->
                 <ul id="side-menu" class="sidebar-menu">
-                    <li class="dropdown"><a href="#"><i class="icon-home mr-1"></i> Katalog</a>                  
+                    <li class="dropdown"><a href="#"><i class="icon-doc mr-1"></i> Katalog</a>                  
                         <ul>
-                            <li class="<?= $this->uri->segment(2) == 'produk' ? 'active' : ''; ?>"><a href="<?= base_url('admin/produk') ?>"><i class="icon-rocket"></i> Produk</a></li>
-                            <li class="<?= $this->uri->segment(2) == 'kategori' ? 'active' : ''; ?>"><a href="<?= base_url('admin/kategori') ?>"><i class="icon-layers"></i> Kategori Produk</a></li>
+                            <li class="<?= $this->uri->segment(2) == 'produk' ? 'active' : ''; ?>"><a href="<?= base_url('admin/produk') ?>"><i class="icon-bag"></i> Produk</a></li>
+                            <li class="<?= $this->uri->segment(2) == 'kategori' ? 'active' : ''; ?>"><a href="<?= base_url('admin/kategori') ?>"><i class="icon-tag"></i> Kategori Produk</a></li>
                         </ul>
                     </li>
-                    <li class="dropdown"><a href="#"><i class="icon-home mr-1"></i> Laporan</a>                  
+                    <li class="dropdown"><a href="#"><i class="icon-book-open mr-1"></i> Laporan</a>                  
                         <ul>
-                            <li class="<?= $this->uri->segment(2) == 'penjualan' ? 'active' : ''; ?>"><a href="<?= base_url('admin/laporan/penjualan') ?>"><i class="icon-rocket"></i> Penjualan</a></li>
-                            <li class="<?= $this->uri->segment(2) == 'penjualan_produk' ? 'active' : ''; ?>"><a href="<?= base_url('admin/laporan/penjualan_produk') ?>"><i class="icon-rocket"></i> Penjualan Produk</a></li>
-                            <li class="<?= $this->uri->segment(2) == 'transaksi' ? 'active' : ''; ?>"><a href="<?= base_url('admin/laporan/transaksi') ?>"><i class="icon-rocket"></i> Transaksi</a></li>
+                            <li class="<?= $this->uri->segment(2) == 'penjualan' ? 'active' : ''; ?>"><a href="<?= base_url('admin/laporan/penjualan') ?>"><i class="icon-credit-card"></i> Penjualan</a></li>
+                            <li class="<?= $this->uri->segment(2) == 'penjualan_produk' ? 'active' : ''; ?>"><a href="<?= base_url('admin/laporan/penjualan_produk') ?>"><i class="icon-event"></i> Penjualan Produk</a></li>
+                            <li class="<?= $this->uri->segment(2) == 'transaksi' ? 'active' : ''; ?>"><a href="<?= base_url('admin/laporan/transaksi') ?>"><i class="icon-pencil"></i> Transaksi</a></li>
                         </ul>
                     </li>
-                    <li class="dropdown"><a href="#"><i class="icon-home mr-1"></i> Data Master</a>                  
+                    <li class="dropdown"><a href="#"><i class="icon-folder mr-1"></i> Data Master</a>                  
                         <ul>
                             <li class="<?= $this->uri->segment(2) == 'pengguna' ? 'active' : ''; ?>"><a href="<?= base_url('admin/pengguna') ?>"><i class="icon-people"></i> Pengguna</a></li>
                         </ul>
